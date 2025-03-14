84423b5b5c02b3a57e3323aba57970972ebfd4c6
hubertdeveloper <hubertzkontakt@proton.me>
2025-03-14 09:57:18 +0100

    drobna zmiana

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Pobierz canvas i kontekst
+// Pobierz canvas i punkty
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const scoreElement = document.getElementById('score');
